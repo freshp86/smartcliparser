c48dd655e7cab4207c8b7dde7af6300b80e2ea94
Demetrios Papadopoulos <dpapad@chromium.org>
2012-01-28 23:57:15 -0800

    Adding demo command line program for reversing a string.

---
+++ src/org/smartcliparser/demos/Reverse.java
@@ -0,0 +1,55 @@
+package org.smartcliparser.demos;
+
+import java.util.Iterator;
+
+import org.smartcliparser.Flag;
+import org.smartcliparser.CommandLineProgram;
+
+public class Reverse extends CommandLineProgram {
+
+  private Flag inputFlag;
+  private Flag lowercaseFlag;
+  private Flag uppercaseFlag;
+
+  public Reverse(String[] args) {
+    super(args);
+  }
+
+  public void initialize() {
+    // Specifying flags.
+    inputFlag = new Flag(new String[]{ "input", "i" }, true, 1,
+      Flag.UNLIMITED_NUM_OF_ARGS);
+    uppercaseFlag = new Flag(new String[]{ "uppercase", "u" }, false, 0, 0);
+    lowercaseFlag = new Flag(new String[]{ "lowercase", "l" }, false, 0, 0);
+
+    // Registering all flags so that the program knows how to parse them.
+    registerFlag(inputFlag);
+    registerFlag(uppercaseFlag);
+    registerFlag(lowercaseFlag);
+  }
+
+  public void run() {
+    Iterator<String> it = inputFlag.args.iterator();
+    while (it.hasNext()) {
+      String reversed = this.reverse(it.next());
+      if (lowercaseFlag.isSet())
+        reversed = reversed.toLowerCase();
+      else if (uppercaseFlag.isSet())
+        reversed = reversed.toUpperCase();
+      System.out.println(reversed);
+    }
+  }
+
+  private String reverse(String string) {
+    StringBuffer out = new StringBuffer();
+    for (int i = string.length() - 1; i >= 0; i--)
+      out.append(string.charAt(i));
+    return out.toString();
+  }
+
+  public static void main(String[] args) {
+    Reverse reverse = new Reverse(args);
+  }
+
+}  // class Reverse
+
